e73cbd32a7a249d34bfe9dde19ca3d7730abdc58
tlmn <mail@tlmn.io>
2021-12-16 15:54:15 +0100

    init mobile controls for Diskursintervention

---
+++ src/templates/diskursintervention/controlsMobile.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import MobileInputMenu from "../../components/mobileInputMenu";
+import MenuItem from "../../components/menuItem";
+import FieldSet from "../../components/fieldSet";
+import Input from "../../components/inputs/input";
+import Textarea from "../../components/inputs/textarea";
+import TextScale from "../../components/inputs/textScale";
+import DownloadButton from "../../components/inputs/downloadButton";
+import ColorThemesSelect from "../../components/inputs/colorThemesSelect";
+
+const ControlsMobile = () => {
+  const currentSlide = 0;
+  return (
+    <div className="block md:hidden fixed bottom-0 w-full left-0 z-20">
+      <FieldSet name={`colorTheme`}>
+        <ColorThemesSelect />
+      </FieldSet>
+      <FieldSet name={`slides[${currentSlide}].data.category`}>
+        <Input
+          label="Titel"
+          propertyPath={`slides[${currentSlide}].data.category.content`}
+        />
+      </FieldSet>
+
+      <FieldSet name={`slides[${currentSlide}].data.body`}>
+        <Textarea
+          label="Text"
+          propertyPath={`slides[${currentSlide}].data.body.content`}
+          rows={3}
+          cols={30}
+        />
+        <TextScale propertyPath={`slides[${currentSlide}].data.body.scale`} />
+      </FieldSet>
+
+      <FieldSet name={`slides[${currentSlide}].data.localBranch`}>
+        <Textarea
+          label="Lokalgruppe (optional)"
+          propertyPath={`slides[${currentSlide}].data.localBranch.content`}
+          rows={3}
+        />
+      </FieldSet>
+
+      <MobileInputMenu>
+        <DownloadButton className="m-1" />
+        <MenuItem
+          legend={`Farbe`}
+          iconType={`color`}
+          targetFieldset={`colorTheme`}
+        />
+        <MenuItem
+          legend={`Titel`}
+          iconType={`title`}
+          targetFieldset={`slides[${currentSlide}].data.category`}
+        />
+        <MenuItem
+          legend={`Text`}
+          iconType={`text`}
+          targetFieldset={`slides[${currentSlide}].data.body`}
+        />
+        <MenuItem
+          legend={`Lokal&shy;gruppe`}
+          iconType={`localGroup`}
+          targetFieldset={`slides[${currentSlide}].data.localBranch`}
+        />
+      </MobileInputMenu>
+    </div>
+  );
+};
+
+export default ControlsMobile;
