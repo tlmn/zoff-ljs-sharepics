e8e715fd89a80678fa207de5dc8b570e44b273fc
tlmn <mail@tlmn.io>
2021-12-16 18:28:03 +0100

    fix landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,30 +5,29 @@ import { templates } from "../config/vars";
 
 const PageIndex = () => (
   <Layout>
-    <div className="bg-darkGray font-headline leading-normal">
-      <div className="container py-2">
-        <div className="mx-auto grid grid-cols-12 gap-x-4 gap-y-2 py-5">
-          <div className="col-span-12 text-center">
-            <h1 className="text-xl text-green uppercase leading-tight italic">
-              Sharepic Generator <br /> linksjugend ['solid]
-            </h1>
-          </div>
-          {templates.map(({ link, name, thumbnailSrc }) => (
-            <div className="col-span-12 sm:col-span-4 text-center">
-              <Link to={link} className="no-underline hover:underline">
-                <span className="block text-base text-white uppercase pb-1">
-                  {name}
-                </span>
-                <img
-                  src={thumbnailSrc}
-                  alt={name}
-                  className="hover:opacity-75 transition-all ease-in-out duration-200"
-                />
-              </Link>
-            </div>
-          ))}
-        </div>
+    <div className="container px-2 grid grid-cols-12 gap-2  font-headline">
+      <div className="col-span-full text-center">
+        <h1 className="text-xl text-green uppercase leading-tight italic py-3">
+          Sharepic Generator <br /> linksjugend ['solid]
+        </h1>
       </div>
+      {templates.map(({ link, name, thumbnailSrc }) => (
+        <div className="col-span-6 sm:col-span-4 text-center">
+          <Link
+            to={link}
+            className="no-underline hover:text-green max-w-full flex flex-col md:flex-col-reverse "
+          >
+            <img
+              src={thumbnailSrc}
+              alt={name}
+              className="hover:opacity-75 transition-all ease-in-out duration-200 w-full"
+            />
+            <span className="block text-base text-white uppercase pb-1 break-all leading-tight mt-1">
+              {name}
+            </span>
+          </Link>
+        </div>
+      ))}
     </div>
   </Layout>
 );
