94815a980c18811eab7dca2d9c2f05676eff28ca
tlmn <mail@tlmn.io>
2021-12-16 19:45:33 +0100

    add mobile controls

--- src/pages/templates/veranstaltung/index.jsx
+++ src/pages/templates/veranstaltung/index.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 
 import ControlsLeft from "../../../templates/veranstaltung/controlsLeft";
 import ControlsRight from "../../../templates/veranstaltung/controlsRight";
+import ControlsMobile from "../../../templates/veranstaltung/controlsMobile";
 import Template from "../../../templates/veranstaltung/template";
 import { Provider as DataContextProvider } from "../../../lib/useDataContext";
 import TemplateLayout from "../../../components/templateLayout";
@@ -9,6 +10,7 @@ import TemplateLayout from "../../../components/templateLayout";
 const PageVeranstaltung = () => {
   const [state, setState] = useState({
     currentSlide: 0,
+    activeFieldset: "",
     slides: [
       {
         data: {
@@ -32,15 +34,16 @@ const PageVeranstaltung = () => {
   return (
     <DataContextProvider value={{ state, setState }}>
       <TemplateLayout>
-        <div className="col-span-3">
+        <div className="col-span-3 hidden md:block">
           <ControlsLeft />
         </div>
-        <div className="col-span-6">
+        <div className="col-span-full md:col-span-6 mx-2 md:p-0">
           <Template />
         </div>
-        <div className="col-span-3">
+        <div className="col-span-3 hidden md:block">
           <ControlsRight />
         </div>
+        <ControlsMobile />
       </TemplateLayout>
     </DataContextProvider>
   );
