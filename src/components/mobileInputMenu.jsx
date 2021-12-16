107dd02fbd3540fa3c7999c6415a423c2622a333
tlmn <mail@tlmn.io>
2021-12-16 19:02:23 +0100

    remove pb-1

--- src/components/mobileInputMenu.jsx
+++ src/components/mobileInputMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MobileInputMenu = ({ children }) => {
   return (
-    <div className="flex gap-1 justify-center overflow-x-scroll overflow-y-hidden bg-black px-1 pb-1">
+    <div className="flex gap-1 justify-center overflow-x-scroll overflow-y-hidden bg-black px-1">
       {children}
     </div>
   );
