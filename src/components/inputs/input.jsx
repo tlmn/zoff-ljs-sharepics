db5d590a266b7fc463e8eb0ffceed05fe0a16b2f
tlmn <mail@tlmn.io>
2021-12-16 13:04:23 +0100

    add erase functionality

--- src/components/inputs/input.jsx
+++ src/components/inputs/input.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getProperty, updateProperty } from "../../lib/lib";
+import EraseIcon from "../../assets/svg/inputIcons/erase";
 
 import useDataContext from "../../lib/useDataContext";
 
@@ -8,15 +9,24 @@ const Input = ({ propertyPath, label, ...props }) => {
   return (
     <>
       <label htmlFor={propertyPath}>{label}</label>
-      <input
-        type="text"
-        value={getProperty({ state }, propertyPath)}
-        onChange={(e) =>
-          updateProperty({ setState }, propertyPath, e.target.value)
-        }
-        id={propertyPath}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          type="text"
+          className="pr-5"
+          value={getProperty({ state }, propertyPath)}
+          onChange={(e) =>
+            updateProperty({ setState }, propertyPath, e.target.value)
+          }
+          id={propertyPath}
+          {...props}
+        />
+        <button
+          className="absolute top-0 bottom-0 my-auto right-1 fill-lightGray hover:fill-turquoise"
+          onClick={() => updateProperty({ setState }, propertyPath, "")}
+        >
+          <EraseIcon />
+        </button>
+      </div>
     </>
   );
 };
