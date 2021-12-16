e9e085d6dfc9f63f3fc228b8a4cb34ef90de4c97
tlmn <mail@tlmn.io>
2021-12-16 18:39:45 +0100

    add mobile controls

--- src/pages/templates/zitat/index.jsx
+++ src/pages/templates/zitat/index.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 
 import ControlsLeft from "../../../templates/zitat/controlsLeft";
 import ControlsRight from "../../../templates/zitat/controlsRight";
+import ControlsMobile from "../../../templates/zitat/controlsMobile";
 import Template from "../../../templates/zitat/template";
 import { Provider as DataContextProvider } from "../../../lib/useDataContext";
 import TemplateLayout from "../../../components/templateLayout";
@@ -9,6 +10,7 @@ import TemplateLayout from "../../../components/templateLayout";
 const PageZitat = () => {
   const [state, setState] = useState({
     currentSlide: 0,
+    activeFieldset: "",
     slides: [
       {
         data: {
@@ -29,15 +31,16 @@ const PageZitat = () => {
   return (
     <DataContextProvider value={{ state, setState }}>
       <TemplateLayout>
-        <div className="col-span-3">
+        <div className="col-span-3 hidden md:block">
           <ControlsLeft />
         </div>
-        <div className="col-span-6">
+        <div className="col-span-full md:col-span-6 mx-2 md:p-0">
           <Template />
         </div>
-        <div className="col-span-3">
+        <div className="col-span-3 hidden md:block">
           <ControlsRight />
         </div>
+        <ControlsMobile />
       </TemplateLayout>
     </DataContextProvider>
   );
