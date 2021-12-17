e964edd47ebc85e83937bbdd91bfd788a0359813
tlmn <mail@tlmn.io>
2021-12-17 12:17:38 +0100

    init mobile controls

---
+++ src/templates/selbstrepraesentation/slide-0/controlsMobile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import MobileInputMenu from "../../../components/mobileInputMenu";
+import MenuItem from "../../../components/menuItem";
+import FieldSet from "../../../components/fieldSet";
+import Input from "../../../components/inputs/input";
+import TextScale from "../../../components/inputs/textScale";
+import DownloadButton from "../../../components/inputs/downloadButton";
+import ColorThemesSelect from "../../../components/inputs/colorThemesSelect";
+
+const ControlsMobile = () => {
+  const currentSlide = 0;
+  return (
+    <div className="block md:hidden absolute bottom-0 w-full left-0 z-20">
+      <div className="flex w-full justify-center">
+        <FieldSet name={`colorTheme`}>
+          <ColorThemesSelect />
+        </FieldSet>
+
+        <FieldSet name={`slides[${currentSlide}].data.body`}>
+          <Input
+            label="Titel"
+            propertyPath={`slides[${currentSlide}].data.body.content`}
+          />
+          <TextScale propertyPath={`slides[${currentSlide}].data.body.scale`} />
+        </FieldSet>
+      </div>
+
+      <MobileInputMenu>
+        <DownloadButton className="m-1" />
+        <MenuItem
+          legend={`Farbe`}
+          iconType={`color`}
+          targetFieldset={`colorTheme`}
+        />
+        <MenuItem
+          legend={`Text`}
+          iconType={`text`}
+          targetFieldset={`slides[${currentSlide}].data.body`}
+        />
+      </MobileInputMenu>
+    </div>
+  );
+};
+
+export default ControlsMobile;
---
+++ src/templates/selbstrepraesentation/slide-1/controlsMobile.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import MobileInputMenu from "../../../components/mobileInputMenu";
+import MenuItem from "../../../components/menuItem";
+import FieldSet from "../../../components/fieldSet";
+import BgImage from "../../../components/inputs/bgImage";
+import DownloadButton from "../../../components/inputs/downloadButton";
+import ColorThemesSelect from "../../../components/inputs/colorThemesSelect";
+
+const ControlsMobile = () => {
+  const currentSlide = 1;
+  return (
+    <div className="block md:hidden absolute bottom-0 w-full left-0 z-20">
+      <div className="flex w-full justify-center">
+        <FieldSet name={`colorTheme`}>
+          <ColorThemesSelect />
+        </FieldSet>
+
+        <FieldSet name={`slides[${currentSlide}].data.image`}>
+          <BgImage currentSlide={currentSlide} />
+        </FieldSet>
+      </div>
+
+      <MobileInputMenu>
+        <DownloadButton className="m-1" />
+        <MenuItem
+          legend={`Farbe`}
+          iconType={`color`}
+          targetFieldset={`colorTheme`}
+        />
+        <MenuItem
+          legend={`Bild`}
+          iconType={`image`}
+          targetFieldset={`slides[${currentSlide}].data.image`}
+        />
+      </MobileInputMenu>
+    </div>
+  );
+};
+
+export default ControlsMobile;
