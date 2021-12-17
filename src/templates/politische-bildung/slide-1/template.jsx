aa680d4a4ec8d819e4294166c1b4a1c25b641290
tlmn <mail@tlmn.io>
2021-12-17 16:07:45 +0100

    use templateWrapper component

--- src/templates/politische-bildung/slide-1/template.jsx
+++ src/templates/politische-bildung/slide-1/template.jsx
@@ -2,34 +2,25 @@ import React from "react";
 
 import useDataContext from "../../../lib/useDataContext";
 import { getPrimaryColor } from "../../../lib/lib";
+import TemplateWrapper from "../../../components/templateWrapper";
 
 const Template = ({ thumbnail = false }) => {
   const currentSlide = 1;
   const { state } = useDataContext();
   return (
-    <div className={`${!thumbnail ? `col-span-6` : ``} relative`}>
-      <div
-        className={`template ${
-          state.templateScale && !thumbnail ? `template-scale` : `relative`
-        }`}
-        style={{
-          backgroundColor: getPrimaryColor(state),
-        }}
-        ref={!thumbnail ? state.slides[currentSlide].ref : null}
-      >
-        <div className="p-4 flex flex-col h-full">
-          <span
-            className="flex-1 font-body font-semibold text-left text-black break-word overflow-hidden"
-            style={{
-              fontSize: `${state.slides[currentSlide].data.body.scale.value}px`,
-            }}
-            dangerouslySetInnerHTML={{
-              __html: state.slides[currentSlide].data.body.content,
-            }}
-          />
-        </div>
+    <TemplateWrapper bgColor={getPrimaryColor(state)} isThumbnail={thumbnail}>
+      <div className="p-4 flex flex-col h-full">
+        <span
+          className="flex-1 font-body font-semibold text-left text-black break-word overflow-hidden"
+          style={{
+            fontSize: `${state.slides[currentSlide].data.body.scale.value}px`,
+          }}
+          dangerouslySetInnerHTML={{
+            __html: state.slides[currentSlide].data.body.content,
+          }}
+        />
       </div>
-    </div>
+    </TemplateWrapper>
   );
 };
 
