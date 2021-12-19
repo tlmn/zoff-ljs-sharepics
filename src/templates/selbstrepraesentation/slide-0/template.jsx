9b70e4902420b960df0eae296f889b0835ab4014
tlmn <mail@tlmn.io>
2021-12-19 12:13:01 +0100

    use templateWrappe component

--- src/templates/selbstrepraesentation/slide-0/template.jsx
+++ src/templates/selbstrepraesentation/slide-0/template.jsx
@@ -1,62 +1,43 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
+import TemplateWrapper from "../../../components/templateWrapper";
 import { formatEmojis, getColor } from "../../../lib/lib";
 
 import useDataContext from "../../../lib/useDataContext";
-import useWindowResize from "../../../lib/useWindowResize";
 
 const Template = ({ thumbnail = false }) => {
-  const { state, setState } = useDataContext();
-  const ref = useRef(null);
-  const { width } = useWindowResize();
+  const { state } = useDataContext();
 
-  useEffect(() => {
-    setState((prev) => ({
-      ...prev,
-      scaleFactor: ref?.current?.clientWidth / 1080,
-    }));
-  }, [ref, width]);
-
-  const { scaleFactor } = state;
   return (
-    <div className="relative" ref={ref}>
-      <div
-        className={`p-4 // flex justify-center items-center absolute // border-1 // template origin-top-left ${
-          state.templateScale && !thumbnail ? `` : `relative`
-        }`}
-        style={{
-          backgroundColor: getColor(state, 0),
-          transform:
-            state.templateScale && !thumbnail ? `scale(${scaleFactor})` : ``,
-        }}
-        ref={!thumbnail ? state.slides[0].ref : null}
-      >
-        <div class="stripeContainer">
-          <div class="stripeText" style={{ color: getColor(state, 0) }}>
-            <div
-              className="text-left // font-headline italic font-bold"
-              dangerouslySetInnerHTML={{
-                __html:
-                  state.slides[0].data.body.content === ""
-                    ? "\u00a0"
-                    : formatEmojis(state.slides[0].data.body.content),
-              }}
-              style={{
-                color: getColor(state, 0),
-                fontSize: `${state.slides[0].data.body.scale.value}px`,
-                transform: "rotate(-6deg)",
-              }}
-            />
-          </div>
+    <TemplateWrapper
+      isThumbnail={thumbnail}
+      className="p-4 flex justify-center items-center"
+    >
+      <div class="stripeContainer">
+        <div class="stripeText" style={{ color: getColor(state, 0) }}>
           <div
-            class="stripeElement"
-            style={{
-              transform: "rotate(-6deg)",
-              backgroundColor: getColor(state, 1),
+            className="text-left // font-headline italic font-bold"
+            dangerouslySetInnerHTML={{
+              __html:
+                state.slides[0].data.body.content === ""
+                  ? "\u00a0"
+                  : formatEmojis(state.slides[0].data.body.content),
             }}
-          ></div>
+            style={{
+              color: getColor(state, 0),
+              fontSize: `${state.slides[0].data.body.scale.value}px`,
+              transform: "rotate(-6deg)",
+            }}
+          />
         </div>
+        <div
+          class="stripeElement"
+          style={{
+            transform: "rotate(-6deg)",
+            backgroundColor: getColor(state, 1),
+          }}
+        ></div>
       </div>
-    </div>
+    </TemplateWrapper>
   );
 };
 
--- src/templates/selbstrepraesentation/slide-1/template.jsx
+++ src/templates/selbstrepraesentation/slide-1/template.jsx
@@ -1,57 +1,36 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 
 import DraggableBg from "../../../components/inputs/draggableBg";
 import useDataContext from "../../../lib/useDataContext";
-import useWindowResize from "../../../lib/useWindowResize";
 
 import { getColor } from "../../../lib/lib";
+import TemplateWrapper from "../../../components/templateWrapper";
 
 const Template = ({ thumbnail = false }) => {
-  const { state, setState } = useDataContext();
-  const { scaleFactor } = state;
-  const ref = useRef(null);
-  const { width } = useWindowResize();
+  const { state } = useDataContext();
 
-  useEffect(() => {
-    setState((prev) => ({
-      ...prev,
-      scaleFactor: ref?.current?.clientWidth / 1080,
-    }));
-  }, [ref, width, setState]);
   return (
-    <div className="relative" ref={ref}>
-      <div
-        className={`template origin-top-left ${
-          state.templateScale && !thumbnail ? `template-scale` : `relative`
-        }`}
-        style={{
-          backgroundColor: getColor(state, 0),
-          transform:
-            state.templateScale && !thumbnail ? `scale(${scaleFactor})` : ``,
-        }}
-        ref={!thumbnail ? state.slides[1].ref : null}
-      >
-        {!thumbnail && (
-          <DraggableBg propertyPath="slides[1].data.image.position" />
-        )}
+    <TemplateWrapper isThumbnail={thumbnail} bgColor={getColor(state, 0)}>
+      {!thumbnail && (
+        <DraggableBg propertyPath="slides[1].data.image.position" />
+      )}
 
-        <div
-          className="absolute // top-0 left-0 right-0 // z-10 // w-full h-full"
-          style={{
-            backgroundImage: `url(${
-              state.slides[1].data.image.url !== null
-                ? state.slides[1].data.image.url
-                : "/assets/images/defaultImages/selbstrepraesentation-1.jpg"
-            })`,
-            height: "100%",
-            filter: "grayscale(100%)",
-            backgroundPositionX: `${state.slides[1].data.image.position.x}px`,
-            backgroundPositionY: `${state.slides[1].data.image.position.y}px`,
-            backgroundSize: `${state.slides[1].data.image.scale * 10 + 100}%`,
-          }}
-        />
-      </div>
-    </div>
+      <div
+        className="absolute // top-0 left-0 right-0 // z-10 // w-full h-full"
+        style={{
+          backgroundImage: `url(${
+            state.slides[1].data.image.url !== null
+              ? state.slides[1].data.image.url
+              : "/assets/images/defaultImages/selbstrepraesentation-1.jpg"
+          })`,
+          height: "100%",
+          filter: "grayscale(100%)",
+          backgroundPositionX: `${state.slides[1].data.image.position.x}px`,
+          backgroundPositionY: `${state.slides[1].data.image.position.y}px`,
+          backgroundSize: `${state.slides[1].data.image.scale * 10 + 100}%`,
+        }}
+      />
+    </TemplateWrapper>
   );
 };
 
