f825941973b0a3e8108db3d3d6d6a55ea5cfc061
tlmn <mail@tlmn.io>
2021-12-19 12:53:32 +0100

    add DownloadButton to mobileInputMenu

--- src/components/mobileInputMenu.jsx
+++ src/components/mobileInputMenu.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import DownloadButton from "../components/inputs/downloadButton";
 
 const MobileInputMenu = ({ children }) => {
   return (
     <div className="flex gap-1 justify-center overflow-x-scroll overflow-y-hidden bg-black px-1">
+      <DownloadButton className="m-1" />
       {children}
     </div>
   );
