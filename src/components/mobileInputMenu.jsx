6546fcf520452a5aa6be640c80b3690ee2395d2f
tlmn <mail@tlmn.io>
2021-12-21 14:17:58 +0100

    fix cut off items

--- src/components/mobileInputMenu.jsx
+++ src/components/mobileInputMenu.jsx
@@ -3,9 +3,11 @@ import DownloadButton from "../components/inputs/downloadButton";
 
 const MobileInputMenu = ({ children }) => {
   return (
-    <div className="flex gap-1 justify-center overflow-x-scroll overflow-y-hidden bg-black px-1">
-      <DownloadButton className="m-1" />
-      {children}
+    <div className="bg-black flex justify-center">
+      <div className="mx-auto flex overflow-x-scroll gap-1 px-1">
+        <DownloadButton className="m-1" />
+        {children}
+      </div>
     </div>
   );
 };
